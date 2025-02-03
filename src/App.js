071cb037c3adc162e041bd98b151777484a5e9ed
Chandima0406 <chandima0406sajith@gmail.com>
2025-02-04 00:46:13 +0530

    Install React Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-      
-    </div>
+    <Router>
+      <div>
+        <switch>
+          
+        </switch>
+      </div>
+    </Router>
+    
   );
 }
 
