1c7aa1b2eb503a767b8bcd46f75ecb651016b523
Chandima0406 <chandima0406sajith@gmail.com>
2025-02-04 17:13:12 +0530

    Reeact useState Hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Switch, Link} from "react-router-dom";
-import Home from './Pages/Home';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
 
 function App() {
+  let count = 0;
   return (
-    <Router>
-      <div>
-        <Routes>
+    <>
 
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/about' element={<About/>}></Route>
-          <Route path='/contact' element={<Contact/>}></Route>
+    <span className='title'>My counter</span>
+    <p className='subtitle'>The count is {count}</p>
+    <button className='button'>-</button>
+    <button className='button'>+</button>
 
-        </Routes>
-      </div>
-    </Router>
-    
+    </>
   );
 }
 
